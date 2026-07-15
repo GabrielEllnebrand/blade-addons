0c666cb13e0e2a61de3a6fdd892c4d665f2da8d8
Gabriel Ellnebrand <gabriel.ellnebrand@gmail.com>
2026-07-15 18:31:56 +0200

    - added death messages

--- src/main/java/blade/addon/features/dungeon/DeathMessage.java
+++ src/main/java/blade/addon/features/dungeon/DeathMessage.java
@@ -2,6 +2,7 @@ package blade.addon.features.dungeon;
 
 import blade.addon.utils.Location;
 import blade.addon.utils.config.values.Dungeons;
+import blade.addon.utils.dungeon.DungeonClass;
 import blade.addon.utils.events.Events;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientPacketListener;
@@ -11,11 +12,7 @@ import java.util.regex.Pattern;
 
 public class DeathMessage {
 
-    private static final Pattern[] patterns = new Pattern[]{
-        Pattern.compile("^ ☠ (.*) was killed by *."),
-        Pattern.compile("^ ☠ (.*) died to a *."),
-        Pattern.compile("^ ☠ (.*) fell to their death *."),
-    };
+    private static final Pattern PATTERN = Pattern.compile("^ ☠ (.+) and became a ghost\\.$");
 
     public static void init() {
 
@@ -24,17 +21,17 @@ public class DeathMessage {
             if (!Location.inDungeon() || !Dungeons.sendDeathMessage) return false;
 
             String msg = text.getString();
-            for(Pattern pattern: patterns) {
-                Matcher matcher = pattern.matcher(msg);
-                if (matcher.find()) {
-                    String name = matcher.group(1);
 
-                    ClientPacketListener networkHandler = Minecraft.getInstance().getConnection();
-                    if (networkHandler == null) return false;
-                    networkHandler.sendCommand("pc " + createMessage(name));
-                    return false;
-                }
-            }
+            Matcher matcher = PATTERN.matcher(msg);
+            if (!matcher.find()) return false;
+
+            String message = matcher.group(1);
+            String name = DungeonClass.getNameFromMessage(message);
+            if (name == null) return false;
+
+            ClientPacketListener networkHandler = Minecraft.getInstance().getConnection();
+            if (networkHandler == null) return false;
+            networkHandler.sendCommand("pc " + createMessage(name));
             return false;
         });
 
